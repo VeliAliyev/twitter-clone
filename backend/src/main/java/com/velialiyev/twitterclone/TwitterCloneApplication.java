46795965189d6d2319daee21a4889ab3176c0eb3
VeliAliyev <veli_aliyev@outlook.com>
2022-06-13 09:49:39 +0300

    initial commit

---
+++ backend/src/main/java/com/velialiyev/twitterclone/TwitterCloneApplication.java
@@ -0,0 +1,13 @@
+package com.velialiyev.twitterclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterCloneApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/velialiyev/twitterclone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velialiyev.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
