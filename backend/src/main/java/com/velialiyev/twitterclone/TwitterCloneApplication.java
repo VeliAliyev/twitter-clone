c636f8c3c4212b2d50bfdcad406ccefb2d81743f
VeliAliyev <veli_aliyev@outlook.com>
2022-06-13 12:45:25 +0300

    web configuration added

--- backend/src/main/java/com/velialiyev/twitterclone/TwitterCloneApplication.java
+++ backend/src/main/java/com/velialiyev/twitterclone/TwitterCloneApplication.java
@@ -2,8 +2,10 @@ package com.velialiyev.twitterclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TwitterCloneApplication {
 
     public static void main(String[] args) {
