c5a53d41e5d5dee5436856a21a48d50218305aa5
VeliAliyev <veli_aliyev@outlook.com>
2022-06-28 09:17:23 +0300

    ReplyController

---
+++ backend/src/main/java/com/velialiyev/twitterclone/controller/ReplyController.java
@@ -0,0 +1,12 @@
+package com.velialiyev.twitterclone.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reply")
+public class ReplyController {
+
+}
--- backend/src/main/java/com/velialiyev/twitterclone/controller/TweetController.java
+++ backend/src/main/java/com/velialiyev/twitterclone/controller/TweetController.java
@@ -5,16 +5,19 @@ import com.velialiyev.twitterclone.service.TweetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tweet")
 @RequiredArgsConstructor
 public class TweetController {
 
     private final TweetService tweetService;
 
-
+    @PostMapping("/create")
     private ResponseEntity<HttpStatus> tweet(@RequestBody TweetDto tweetDto){
         this.tweetService.tweet(tweetDto);
         return ResponseEntity.ok().build();
