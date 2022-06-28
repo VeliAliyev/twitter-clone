808e85174b2e6fb237da2b85c76615d104cd90df
VeliAliyev <veli_aliyev@outlook.com>
2022-06-28 09:30:23 +0300

    Delete tweet endpoint

--- backend/src/main/java/com/velialiyev/twitterclone/controller/TweetController.java
+++ backend/src/main/java/com/velialiyev/twitterclone/controller/TweetController.java
@@ -5,10 +5,7 @@ import com.velialiyev.twitterclone.service.TweetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +16,12 @@ public class TweetController {
     @PostMapping("/tweet")
     private ResponseEntity<HttpStatus> tweet(@RequestBody TweetDto tweetDto){
         this.tweetService.tweet(tweetDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/tweet/{id}")
+    private ResponseEntity<HttpStatus> deleteTweet(@PathVariable(name = "id") Long id){
+        this.tweetService.deleteTweet(id);
         return ResponseEntity.ok().build();
     }
 
