956a74b80f38b114c7c0a52822dc4371b18aea33
VeliAliyev <veli_aliyev@outlook.com>
2022-06-29 09:07:58 +0300

    List of tweets and replies for user

--- backend/src/main/java/com/velialiyev/twitterclone/entity/UserEntity.java
+++ backend/src/main/java/com/velialiyev/twitterclone/entity/UserEntity.java
@@ -2,19 +2,17 @@ package com.velialiyev.twitterclone.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +24,10 @@ public class UserEntity {
 
     @NotNull
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<TweetEntity> tweets;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<ReplyEntity> replies;
 }
