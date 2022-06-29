baa3f07b4f4ce8b04ad02bdd813d398130cc8082
VeliAliyev <veli_aliyev@outlook.com>
2022-06-29 09:06:15 +0300

    LikeDto

---
+++ backend/src/main/java/com/velialiyev/twitterclone/dto/LikeDto.java
@@ -0,0 +1,15 @@
+package com.velialiyev.twitterclone.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeDto {
+
+    private Long tweetId;
+    private String type;
+
+}
