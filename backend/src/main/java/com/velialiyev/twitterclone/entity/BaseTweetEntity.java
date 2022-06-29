f7b276c68d4fee11bf691e86e31afe716194b0ed
VeliAliyev <veli_aliyev@outlook.com>
2022-06-29 09:07:20 +0300

    @Getter @Setter

--- backend/src/main/java/com/velialiyev/twitterclone/entity/BaseTweetEntity.java
+++ backend/src/main/java/com/velialiyev/twitterclone/entity/BaseTweetEntity.java
@@ -1,11 +1,15 @@
 package com.velialiyev.twitterclone.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
+@Getter
+@Setter
 @SuperBuilder
 @MappedSuperclass
 @NoArgsConstructor
--- backend/src/main/java/com/velialiyev/twitterclone/entity/TweetEntity.java
+++ backend/src/main/java/com/velialiyev/twitterclone/entity/TweetEntity.java
@@ -1,16 +1,19 @@
 package com.velialiyev.twitterclone.entity;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
-public class TweetEntity extends BaseTweetEntity{
+public class TweetEntity extends BaseTweetEntity {
 
     @OneToMany(mappedBy = "tweet",cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
     private List<ReplyEntity> replies;
