de71905ceffabe634961d3ed6ea00ac1723e1136
VeliAliyev <veli_aliyev@outlook.com>
2022-06-30 16:32:48 +0300

    LikeRepository

---
+++ backend/src/main/java/com/velialiyev/twitterclone/repository/LikeRepository.java
@@ -0,0 +1,12 @@
+package com.velialiyev.twitterclone.repository;
+
+import com.velialiyev.twitterclone.entity.LikeEntity;
+import com.velialiyev.twitterclone.entity.TweetEntity;
+import com.velialiyev.twitterclone.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
+    Optional<LikeEntity> findByUserAndTweet(UserEntity user, TweetEntity tweet);
+}
