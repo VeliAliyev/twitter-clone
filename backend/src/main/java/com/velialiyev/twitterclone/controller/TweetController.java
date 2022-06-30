8eaea28f087204116b062438b3ea92736c35eb77
VeliAliyev <veli_aliyev@outlook.com>
2022-06-30 16:33:15 +0300

    Endpoints to delete and to like a tweet

--- backend/src/main/java/com/velialiyev/twitterclone/controller/TweetController.java
+++ backend/src/main/java/com/velialiyev/twitterclone/controller/TweetController.java
@@ -1,14 +1,12 @@
 package com.velialiyev.twitterclone.controller;
 
+import com.velialiyev.twitterclone.dto.LikeDto;
 import com.velialiyev.twitterclone.dto.TweetDto;
 import com.velialiyev.twitterclone.service.TweetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tweet")
@@ -19,8 +17,19 @@ public class TweetController {
 
     @PostMapping("/post")
     public ResponseEntity<HttpStatus> createTweet(@RequestBody TweetDto tweetDto){
-        this.tweetService.createTweet(tweetDto);
+        this.tweetService.tweet(tweetDto);
         return ResponseEntity.ok().build();
     }
 
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteTweet(@PathVariable(name = "id") Long id){
+        this.tweetService.deleteTweet(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/like")
+    public ResponseEntity<HttpStatus> like(@RequestBody LikeDto likeDto){
+        this.tweetService.like(likeDto);
+        return ResponseEntity.ok().build();
+    }
 }
