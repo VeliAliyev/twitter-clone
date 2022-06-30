6bf590f384c9a150a0976640e461102f78e71db4
VeliAliyev <veli_aliyev@outlook.com>
2022-06-30 13:56:04 +0300

    List of replies in TweetEntity

--- backend/src/main/java/com/velialiyev/twitterclone/entity/TweetEntity.java
+++ backend/src/main/java/com/velialiyev/twitterclone/entity/TweetEntity.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.Instant;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,16 +17,23 @@ public class TweetEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private UserEntity user;
+
     private String text;
     private Integer replyCounter;
     private Integer retweetCounter;
     private Integer likeCounter;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tweet_id", referencedColumnName = "id")
     private TweetEntity tweet;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    private List<TweetEntity> replies;
+
     private TweetType type;
     private Instant createdDate;
 }
