0f864bac95fb32e669d501ef39fc6863157675df
VeliAliyev <veli_aliyev@outlook.com>
2022-07-02 19:27:31 +0300

    app.module updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignInComponent } from './landing-page/sign-in/sign-in.component';
+import { SignUpComponent } from './landing-page/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "" , component: LandingPageComponent},
+  {path : "signin" , component: SignInComponent},
+  {path : "signup" , component: SignUpComponent},
+  {path : "home" , component: HomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SignInComponent } from './landing-page/sign-in/sign-in.component';
+import { SignUpComponent } from './landing-page/sign-up/sign-up.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
