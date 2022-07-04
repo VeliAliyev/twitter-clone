0c2feb50cc76f8373ac585e16d7e4beba1904bc2
VeliAliyev <veli_aliyev@outlook.com>
2022-07-04 18:46:27 +0300

    TweetResponseDto

---
+++ backend/src/main/java/com/velialiyev/twitterclone/dto/TweetResponseDto.java
@@ -0,0 +1,20 @@
+package com.velialiyev.twitterclone.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TweetResponseDto {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String duration;
+    private String tweetText;
+    private Integer replyCounter;
+    private Integer retweetCounter;
+    private Integer likeCounter;
+
+}
