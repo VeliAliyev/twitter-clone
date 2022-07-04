8dfeb7ca085a84e0e1b0ad30ce3d96aa02a98abe
VeliAliyev <veli_aliyev@outlook.com>
2022-07-04 18:49:04 +0300

    home-page component updated

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { TweetRequestPayload } from './tweet-request.payload';
+import { TweetResponsePayload } from './tweet-response.payload';
+import { TweetService } from './tweet.service';
 
 @Component({
   selector: 'app-home-page',
@@ -8,14 +12,69 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  navigationSubscription;
+  newTweetForm: FormGroup;
+  tweetRequestPayload: TweetRequestPayload;
+  tweets: Array<TweetResponsePayload>;
+  constructor(private activatedRoute: ActivatedRoute, private toastr: ToastrService, private tweetService: TweetService, private router: Router) { 
+    
+    this.navigationSubscription = this.router.events.subscribe((event: any)=>{
+      if(event instanceof NavigationEnd){
+        this.fetchTweets();
+      }
+    })
+    
+    this.newTweetForm = new FormGroup({
+      text: new FormControl("")
+    })
 
-  constructor(private activatedRoute: ActivatedRoute, private toastr: ToastrService) { }
+    this.tweets = new Array();
+
+    this.tweetRequestPayload = {
+      text: "",
+      type: ""
+    }
+
+  }
 
   ngOnInit(): void {
 
     this.activatedRoute.queryParams.subscribe(params=>{
       if(params["signedIn"] !== undefined && params["signedIn"] === "true"){
         this.toastr.success("Sign In Successful");
+      }
+    })
+    this.fetchTweets();
+  }
+
+  fetchTweets(){
+    const self = this;
+    this.tweetService.getAllTweets().subscribe({
+      next(data){
+        self.tweets = data;
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
+  tweet(){
+    
+    this.tweetRequestPayload.text = this.newTweetForm.get("text")?.value;
+    this.tweetRequestPayload.type = "TWEET";
+    const self = this;
+    this.tweetService.tweet(this.tweetRequestPayload).subscribe({
+      next(response){
+        console.log(response)
+        self.newTweetForm.reset();
+        self.router.navigateByUrl("home");
+      },
+      complete(){},
+      error(error){
+        console.log(error)
       }
     })
 
