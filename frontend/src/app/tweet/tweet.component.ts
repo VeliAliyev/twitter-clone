ce99fd25fc4e0f84088987c3636938f266c1ca20
VeliAliyev <veli_aliyev@outlook.com>
2022-07-11 15:21:46 +0300

    tweet component updated

---
+++ frontend/src/app/tweet/tweet.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TweetResponsePayload } from '../home-page/tweet-response.payload';
+
+@Component({
+  selector: 'app-tweet',
+  templateUrl: './tweet.component.html',
+  styleUrls: ['./tweet.component.css']
+})
+export class TweetComponent implements OnInit {
+  @Input() tweet: TweetResponsePayload;
+  constructor(private router: Router) { 
+    this.tweet = {
+      id: 0,
+      firstName: "",
+      lastName: "",
+      username: "",
+      duration: "",
+      tweetText: "",
+      replyCounter: 0,
+      retweetCounter: 0,
+      likeCounter: 0,
+    
+      }
+  }
+
+  ngOnInit(): void {
+  }
+
+  goToTweet(tweetId: number){
+    this.router.navigateByUrl("/tweet/" + tweetId);
+  }
+}
