f7121475338c756fe6b6505495c06a2d989a7138
VeliAliyev <veli_aliyev@outlook.com>
2022-07-12 19:49:18 +0300

    RetweetRepository findAllByUser

--- backend/src/main/java/com/velialiyev/twitterclone/repository/RetweetRepository.java
+++ backend/src/main/java/com/velialiyev/twitterclone/repository/RetweetRepository.java
@@ -5,9 +5,11 @@ import com.velialiyev.twitterclone.entity.TweetEntity;
 import com.velialiyev.twitterclone.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RetweetRepository extends JpaRepository<RetweetEntity, Long> {
     Optional<RetweetEntity> findByUserAndTweet(UserEntity user, TweetEntity tweet);
     Optional<RetweetEntity> findAllByTweet(TweetEntity tweet);
+    Optional<List<RetweetEntity>> findAllByUser(UserEntity user);
 }
