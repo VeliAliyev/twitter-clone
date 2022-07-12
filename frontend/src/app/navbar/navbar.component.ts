e4af5297cfa53e724841a155ebfcf466ce8c8ba0
VeliAliyev <veli_aliyev@outlook.com>
2022-07-12 19:54:08 +0300

    navbar updated

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  url: string;
+
+  constructor(private router: Router) {
+    this.url = this.router.url;
+    console.log("URL : " + this.url)
+   }
 
   ngOnInit(): void {
   }
