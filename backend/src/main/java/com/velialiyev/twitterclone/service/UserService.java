75a461dbc8cd1437be7ea16fc12a58b890c84a70
VeliAliyev <veli_aliyev@outlook.com>
2022-07-14 20:33:16 +0300

    UserService

--- backend/src/main/java/com/velialiyev/twitterclone/service/UserService.java
+++ backend/src/main/java/com/velialiyev/twitterclone/service/UserService.java
@@ -1,42 +1,43 @@
 package com.velialiyev.twitterclone.service;
 
 import com.velialiyev.twitterclone.dto.UserDto;
+import com.velialiyev.twitterclone.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final AuthenticationService authenticationService;
 
-    public void saveImage(UserDto user) {
-        String folder = "/uploads/";
+    public void saveImage(MultipartFile picture) {
+        UserEntity user = this.authenticationService.getUserFromJwt();
 
-        String pictureName = user.getUsername()+ "." + FilenameUtils.getExtension(user.getProfilePicture().getOriginalFilename());
-        String pictureFolder = folder.concat("picture/" + user.getUsername() + "/");
+        Path path = Paths.get("/uploads/picture/" + user.getUsername());
+        String pictureName = "/" + user.getUsername()+ "." + FilenameUtils.getExtension(picture.getOriginalFilename());
 
-        Path path = Paths.get(pictureFolder);
         File directory = new File(path.toString());
         if(!directory.exists()){
             directory.mkdirs();
         }
 
         try {
-            byte[] pictureBytes = user.getProfilePicture().getBytes();
-
-            Path picturePath = Paths.get(pictureFolder + pictureName);
+            byte[] pictureBytes = picture.getBytes();
+            System.out.println("PATH : " + path);
+            System.out.println("Picture Name : " + pictureName);
+            Path picturePath = Paths.get(path + pictureName);
+            System.out.println("Picture Path : " + picturePath);
             FileUtils.cleanDirectory(directory);
             Files.write(picturePath, pictureBytes);
 
@@ -44,5 +45,16 @@ public class UserService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public UserDto getCurrentUser() {
+
+        UserEntity user = this.authenticationService.getUserFromJwt();
+
+        return UserDto.builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .username(user.getUsername())
+                .build();
     }
 }
