ede481423de387147b716def7a3998fea94f6056
VeliAliyev <veli_aliyev@outlook.com>
2022-07-17 21:31:10 +0300

    UserController

--- backend/src/main/java/com/velialiyev/twitterclone/controller/UserController.java
+++ backend/src/main/java/com/velialiyev/twitterclone/controller/UserController.java
@@ -3,10 +3,14 @@ package com.velialiyev.twitterclone.controller;
 import com.velialiyev.twitterclone.dto.UserDto;
 import com.velialiyev.twitterclone.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
 
 
 @RestController
@@ -15,18 +19,53 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
 
     private final UserService userService;
+    String profilePictureDirectory = "/profilePicture";
+    String bannerPictureDirectory = "/bannerPicture";
 
-    @PostMapping("/edit")
-    public ResponseEntity<HttpStatus> editProfile(@RequestParam(name = "profilePicture") MultipartFile image){
+    @GetMapping("/profile-picture/get/{username}")
+    public ResponseEntity<?> getProfilePicture(@PathVariable(name = "username") String username){
 
-       this.userService.saveImage(image);
+        Path path = this.userService.fetchPicturePath(username, this.profilePictureDirectory);
+        ByteArrayResource resource = this.userService.getPicture(username, this.profilePictureDirectory);
+
+        return ResponseEntity.ok().contentLength(path.toFile().length()).contentType(MediaType.IMAGE_JPEG).body(resource);
+    }
+
+    @PostMapping("/profile-picture/save")
+    public ResponseEntity<HttpStatus> saveProfilePicture(
+            @RequestParam(name = "profilePicture") MultipartFile profilePicture,
+            @RequestParam(name = "username") String username){
+
+       this.userService.savePicture(profilePicture, username, this.profilePictureDirectory);
 
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public ResponseEntity<UserDto> getCurrentUser(){
-        return ResponseEntity.ok(this.userService.getCurrentUser());
+    @GetMapping("/banner-picture/get/{username}")
+    public ResponseEntity<?> getBannerPicture(@PathVariable(name = "username") String username){
+        Path path = this.userService.fetchPicturePath(username, this.bannerPictureDirectory);
+        ByteArrayResource resource = this.userService.getPicture(username, this.bannerPictureDirectory);
+
+        return ResponseEntity.ok().contentLength(path.toFile().length()).contentType(MediaType.IMAGE_JPEG).body(resource);
     }
+
+    @PostMapping("/banner-picture/save")
+    public ResponseEntity<HttpStatus> saveBannerPicture(
+            @RequestParam(name = "bannerPicture") MultipartFile bannerPicture,
+            @RequestParam(name = "username") String username){
+
+        this.userService.savePicture(bannerPicture, username, this.bannerPictureDirectory);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping("{username}")
+    public ResponseEntity<UserDto> getCurrentUser(@PathVariable(name = "username") String username){
+        return ResponseEntity.ok(this.userService.getCurrentUser(username));
+    }
+
+
+
 
 }
